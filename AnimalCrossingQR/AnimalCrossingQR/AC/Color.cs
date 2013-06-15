99378e87baf7558d61864e6d1c6ee90c3a40084c
Jefff <gmjefff@gmail.com>
2013-06-14 22:19:29 -0500

    Added Color and Palette classes

---
+++ AnimalCrossingQR/AnimalCrossingQR/AC/Color.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnimalCrossingQR
+{
+    public class Color
+    {
+        public byte Red { get; set; }
+        public byte Green { get; set; }
+        public byte Blue { get; set; }
+
+        public Color(byte red, byte green, byte blue)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+        }
+    }
+}
