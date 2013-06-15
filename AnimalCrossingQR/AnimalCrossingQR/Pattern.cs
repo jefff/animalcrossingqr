61d18388fa277b9a218aa168946b89bae59f3c50
Jefff <gmjefff@gmail.com>
2013-06-14 20:19:25 -0500

    Created initial solution with Pattern and User.

---
+++ AnimalCrossingQR/AnimalCrossingQR/Pattern.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnimalCrossingQR
+{
+    public class Pattern
+    {
+        public const int Width = 32;
+        public const int Height = 32;
+
+        public string Title { get; set; }
+        public byte[,] Data { get; set; }
+
+        public Pattern()
+        {
+            Data = new byte[Width, Height];
+        }
+    }
+}
---
+++ AnimalCrossingQR/AnimalCrossingQR/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnimalCrossingQR
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Town { get; set; }
+        public byte[] UniqueID { get; set; }
+
+        public User(string name, string town, byte[] uniqueID)
+        {
+            Name = name;
+            Town = town;
+            UniqueID = uniqueID;
+        }
+    }
+}
