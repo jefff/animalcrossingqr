c0f12a680039f83838136d119e852de51f5e3d43
Jefff <gmjefff@gmail.com>
2013-06-15 00:24:02 -0500

    Added initial NibbleReader.

--- AnimalCrossingQR/AnimalCrossingQR/AC/Pattern.cs
+++ AnimalCrossingQR/AnimalCrossingQR/AC/Pattern.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -16,6 +17,52 @@ namespace AnimalCrossingQR
         public Pattern()
         {
             Data = new byte[Width, Height];
+        }
+
+        public Pattern(byte[] rawData)
+        {
+
+        }
+
+        private Pattern(BinaryReader binaryReader)
+        {
+
+        }
+    }
+
+    private class NibbleReader
+    {
+        private BinaryReader binaryReader;
+
+        private bool byteAligned = true;
+        private byte halfReadByte;
+
+        public NibbleReader(BinaryReader binaryReader)
+        {
+            this.binaryReader = binaryReader;
+        }
+
+        public byte ReadNibble()
+        {
+            if (byteAligned)
+            {
+                halfReadByte = binaryReader.ReadByte();
+                byteAligned = false;
+                return halfReadByte >> 4;
+            }
+            else
+            {
+                byteAligned = true;
+                return halfReadByte & 0xF;
+            }
+        }
+
+        public byte ReadByte()
+        {
+            byte highNibble = ReadNibble();
+            byte lowNibble = ReadNibble();
+
+            return (highNibble << 4) || lowNibble;
         }
     }
 }
