f5a901c748ef575ef14ecf52bd95a8fad4aab75c
Jefff <gmjefff@gmail.com>
2013-06-17 03:25:11 -0500

    Added selected colors to ColorDialog

--- AnimalCrossingQR/AnimalCrossingQR/ColorDialog.Designer.cs
+++ AnimalCrossingQR/AnimalCrossingQR/ColorDialog.Designer.cs
@@ -28,59 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
             this.colorPanel = new System.Windows.Forms.Panel();
+            this.palettePanel = new System.Windows.Forms.Panel();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.Location = new System.Drawing.Point(12, 12);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(59, 24);
-            this.panel1.TabIndex = 0;
-            // 
-            // panel2
-            // 
-            this.panel2.Location = new System.Drawing.Point(12, 42);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(59, 24);
-            this.panel2.TabIndex = 1;
-            // 
-            // panel3
-            // 
-            this.panel3.Location = new System.Drawing.Point(12, 102);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(59, 24);
-            this.panel3.TabIndex = 3;
-            // 
-            // panel4
-            // 
-            this.panel4.Location = new System.Drawing.Point(12, 72);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(59, 24);
-            this.panel4.TabIndex = 2;
             // 
             // colorPanel
             // 
             this.colorPanel.Location = new System.Drawing.Point(96, 12);
             this.colorPanel.Name = "colorPanel";
-            this.colorPanel.Size = new System.Drawing.Size(491, 430);
+            this.colorPanel.Size = new System.Drawing.Size(322, 308);
             this.colorPanel.TabIndex = 4;
             this.colorPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.colorPanel_Paint);
+            // 
+            // palettePanel
+            // 
+            this.palettePanel.Location = new System.Drawing.Point(12, 13);
+            this.palettePanel.Name = "palettePanel";
+            this.palettePanel.Size = new System.Drawing.Size(78, 307);
+            this.palettePanel.TabIndex = 5;
+            this.palettePanel.Paint += new System.Windows.Forms.PaintEventHandler(this.palettePanel_Paint);
             // 
             // ColorDialog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(617, 484);
+            this.ClientSize = new System.Drawing.Size(430, 332);
+            this.Controls.Add(this.palettePanel);
             this.Controls.Add(this.colorPanel);
-            this.Controls.Add(this.panel3);
-            this.Controls.Add(this.panel4);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "ColorDialog";
             this.Text = "Edit Palette";
@@ -91,10 +65,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.Panel colorPanel;
+        private System.Windows.Forms.Panel palettePanel;
     }
 }
--- AnimalCrossingQR/AnimalCrossingQR/ColorDialog.cs
+++ AnimalCrossingQR/AnimalCrossingQR/ColorDialog.cs
@@ -16,13 +16,46 @@ namespace AnimalCrossingQR
             InitializeComponent();
         }
 
+        private Brush[] oppositePaletteBrushes;
         private Brush[] paletteBrushes;
+        private int[] currentPalette;
+
+        private Font textFont;
+
+        private StringFormat stringFormat;
 
         private const int ColorBoxBorder = 1;
         private const int ColorBoxSize = 20;
         private const int ColorBoxesLeftBorder = 22;
         private const int NineBoxSpacing = 10;
         private const int FirstGrayscaleIndex = 144;
+
+        private void ColorDialog_Load(object sender, EventArgs e)
+        {
+            textFont = new System.Drawing.Font("Arial", 8);
+            stringFormat = new StringFormat()
+            {
+                LineAlignment = StringAlignment.Center,
+                Alignment = StringAlignment.Center,
+            };
+
+            paletteBrushes = Palette.ColorPalette
+                .Select(c => new SolidBrush(System.Drawing.Color.FromArgb(c.Red, c.Green, c.Blue)))
+                .ToArray();
+
+            /*oppositePaletteBrushes = Palette.ColorPalette
+                .Select(c => new SolidBrush(System.Drawing.Color.FromArgb(255 - c.Red, 255 - c.Green, 255 - c.Blue)))
+                .ToArray();*/
+            oppositePaletteBrushes = Palette.ColorPalette
+                .Select(c => (c.Red * 0.2126) + (c.Green * 0.7152) + (c.Blue * 0.0722) > 128 ? Brushes.Black : Brushes.White)
+                .ToArray();
+
+
+            currentPalette = new int[15];
+            Random r = new Random();
+            for (int i = 0; i < 15; i++)
+                currentPalette[i] = r.Next(Palette.ColorPalette.Length);
+        }
 
         private void DrawFullColorPalette(Graphics graphics)
         {
@@ -49,13 +82,17 @@ namespace AnimalCrossingQR
 
             for (int i = 0; i < 3; i++)
                 for (int j = 0; j < 3; j++)
-                    graphics.FillRectangle(
-                        paletteBrushes[colorIndex + 3 * j + i],
-                        new Rectangle(
+                {
+                    Rectangle rect = new Rectangle(
                             x + (ColorBoxSize + ColorBoxBorder) * i,
                             y + (ColorBoxSize + ColorBoxBorder) * j,
                             ColorBoxSize,
-                            ColorBoxSize));
+                            ColorBoxSize);
+
+                    graphics.FillRectangle(paletteBrushes[colorIndex + 3 * j + i], rect);
+
+                    graphics.DrawString("14", textFont, oppositePaletteBrushes[colorIndex + 3 * j + i], rect, stringFormat);
+                }
         }
 
         private void colorPanel_Paint(object sender, PaintEventArgs e)
@@ -63,11 +100,13 @@ namespace AnimalCrossingQR
             DrawFullColorPalette(e.Graphics);
         }
 
-        private void ColorDialog_Load(object sender, EventArgs e)
+        private void palettePanel_Paint(object sender, PaintEventArgs e)
         {
-            paletteBrushes = Palette.ColorPalette
-                .Select(c => new SolidBrush(System.Drawing.Color.FromArgb(c.Red, c.Green, c.Blue)))
-                .ToArray();
+            for (int i = 0; i < currentPalette.Length; i++)
+            {
+                e.Graphics.FillRectangle(i == 0 ? Brushes.Gold : Brushes.Black, new Rectangle(5 - 2, 5 - 2 + 20 * i, 50 + 4, 15 + 4));
+                e.Graphics.FillRectangle(paletteBrushes[currentPalette[i]], new Rectangle(5, 5 + 20 * i, 50, 15));
+            }
         }
     }
 }
