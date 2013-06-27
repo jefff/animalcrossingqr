baa30ccc70a6b1a703ada3c4d920b6f2cb29116f
Jefff <gmjefff@gmail.com>
2013-06-27 02:00:22 -0500

    Added URL Dialog for picking images from URLs

---
+++ AnimalCrossingQR/AnimalCrossingQR/URLDialog.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace AnimalCrossingQR
+{
+    partial class URLDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.urlLabel = new System.Windows.Forms.Label();
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.errorLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.Location = new System.Drawing.Point(34, 52);
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(466, 20);
+            this.urlTextBox.TabIndex = 0;
+            // 
+            // urlLabel
+            // 
+            this.urlLabel.AutoSize = true;
+            this.urlLabel.Location = new System.Drawing.Point(21, 23);
+            this.urlLabel.Name = "urlLabel";
+            this.urlLabel.Size = new System.Drawing.Size(92, 13);
+            this.urlLabel.TabIndex = 1;
+            this.urlLabel.Text = "Enter Image URL:";
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(273, 101);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(95, 30);
+            this.okButton.TabIndex = 2;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(391, 101);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(80, 30);
+            this.cancelButton.TabIndex = 3;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.AutoSize = true;
+            this.errorLabel.Location = new System.Drawing.Point(47, 75);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.Size = new System.Drawing.Size(32, 13);
+            this.errorLabel.TabIndex = 5;
+            this.errorLabel.Text = "Error:";
+            this.errorLabel.Visible = false;
+            // 
+            // URLDialog
+            // 
+            this.AcceptButton = this.okButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 143);
+            this.Controls.Add(this.errorLabel);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.urlLabel);
+            this.Controls.Add(this.urlTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "URLDialog";
+            this.Text = "Enter Image URL:";
+            this.Load += new System.EventHandler(this.URLDialog_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox urlTextBox;
+        private System.Windows.Forms.Label urlLabel;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Label errorLabel;
+    }
+}
---
+++ AnimalCrossingQR/AnimalCrossingQR/URLDialog.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace AnimalCrossingQR
+{
+    public partial class URLDialog : Form
+    {
+        Thread thread;
+        byte[] downloadData;
+
+        public Image ResultImage { get; private set; }
+
+        public URLDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void URLDialog_Load(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsText())
+            {
+                Uri uri;
+                if (Uri.TryCreate(Clipboard.GetText(), UriKind.Absolute, out uri))
+                {
+                    if (uri.Scheme.Equals("http", StringComparison.OrdinalIgnoreCase) ||
+                        uri.Scheme.Equals("https", StringComparison.OrdinalIgnoreCase))
+                        urlTextBox.Text = Clipboard.GetText();
+                }
+            }
+        }
+
+        private void DownloadAsync(string url)
+        {
+            if (thread != null)
+                return;
+
+            thread = new Thread(() =>
+                {
+                    okButton.Invoke((Action)(() => 
+                        {
+                            okButton.Text = "Downloading...";
+                            okButton.Enabled = false;
+                        }));
+
+                    try
+                    {
+                        using (WebClient webClient = new WebClient())
+                            downloadData = webClient.DownloadData(url);
+
+                        MemoryStream ms = new MemoryStream(downloadData);
+                        ResultImage = Bitmap.FromStream(ms);
+
+                        DialogResult = DialogResult.OK;
+                    }
+                    catch (WebException e)
+                    {
+                        errorLabel.Invoke((Action)(() =>
+                            {
+                                errorLabel.Text = "Failed to download image. Check the URL and try again.";
+                                errorLabel.Visible = true;
+                            }));
+                    }
+                    catch (ArgumentException e)
+                    {
+                        errorLabel.Invoke((Action)(() =>
+                        {
+                            errorLabel.Text = "The downloaded file is not a valid image. Check that the URL is a direct image link.";
+                            errorLabel.Visible = true;
+                        }));
+                    }
+                    catch (ThreadAbortException)
+                    {
+                    }
+                    finally
+                    {
+                        thread = null;
+
+                        okButton.Invoke((Action)(() =>
+                        {
+                            okButton.Text = "OK";
+                            okButton.Enabled = true;
+                        }));
+                    }
+                });
+
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            DownloadAsync(urlTextBox.Text);
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            if (thread != null)
+            {
+                thread.Abort();
+                thread = null;
+                return;
+            }
+
+            DialogResult = DialogResult.Cancel;
+        }
+    }
+}
